4ce2c9b0620c0ca9db6cf3016dd39a73da5423f4
ManuelTano <manueltano98@gmail.com>
2022-10-18 17:31:14 +0200

    feat: finished migrations

--- app/User.php
+++ app/User.php
@@ -56,4 +56,9 @@ class User extends Authenticatable
     {
         return $this->hasMany('App\Models\Review');
     }
+
+    public function specialties()
+    {
+        return $this->belongsToMany('App\Models\Specialty');
+    }
 }
