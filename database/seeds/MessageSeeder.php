6c203370064863df9b8ae479fb8681ef81cb6fea
PasqualePedoto <pasquale.pedoto123@gmail.com>
2022-10-19 08:31:13 +0200

    feat: added MessageSeeder

--- database/seeds/MessageSeeder.php
+++ database/seeds/MessageSeeder.php
@@ -1,6 +1,9 @@
 <?php
 
 use Illuminate\Database\Seeder;
+use App\User;
+use App\Models\Message;
+use Faker\Generator as Faker;
 
 class MessageSeeder extends Seeder
 {
@@ -9,8 +12,20 @@ class MessageSeeder extends Seeder
      *
      * @return void
      */
-    public function run()
+    public function run(Faker $faker)
     {
-        //
+        $users = User::all();
+
+        foreach($users as $user){
+            $new_message = new Message();
+
+            $new_message->user_id = $user->id;
+
+            $new_message->first_name = $faker->firstName();
+            $new_message->last_name = $faker->lastName();
+            $new_message->text = $faker->paragraphs();
+
+            $new_message->save();
+        }
     }
 }
