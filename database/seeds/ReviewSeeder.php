d8c33d554638dac4cd3f7766a5fb290c7f5707f4
PasqualePedoto <pasquale.pedoto123@gmail.com>
2022-10-19 09:17:14 +0200

    feat: added ReviewSeeder

--- database/seeds/ReviewSeeder.php
+++ database/seeds/ReviewSeeder.php
@@ -1,6 +1,12 @@
 <?php
 
 use Illuminate\Database\Seeder;
+use Illuminate\Support\Arr;
+
+
+use App\User;
+use App\Models\Review;
+use Faker\Generator as Faker;
 
 class ReviewSeeder extends Seeder
 {
@@ -9,8 +15,21 @@ class ReviewSeeder extends Seeder
      *
      * @return void
      */
-    public function run()
+    public function run(Faker $faker)
     {
-        //
+        $users = User::pluck('id')->toArray();
+
+        foreach($users as $user){
+            $new_review = new Review();
+
+            $new_review->user_id = $user;
+
+            $new_review->first_name = $faker->firstName();
+            $new_review->last_name = $faker->lastName();
+            $new_review->feedback = $faker->paragraph();
+            $new_review->rating = $faker->numberBetween(0, 5);
+
+            $new_review->save();
+        }
     }
 }
