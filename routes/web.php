3bf77e4bcd4eba315ca1858165f9038a73115b55
ManuelTano <manueltano98@gmail.com>
2022-10-19 12:02:16 +0200

    update

--- routes/web.php
+++ routes/web.php
@@ -2,6 +2,7 @@
 
 use Illuminate\Support\Facades\Route;
 use App\Http\Middleware\Authenticate;
+use Illuminate\Support\Facades\Auth;
 
 /*
 |--------------------------------------------------------------------------
