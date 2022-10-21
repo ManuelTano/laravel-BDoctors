cf50951215849f3dd303aba1bfc0698dd0ebacc1
PasqualePedoto <pasquale.pedoto123@gmail.com>
2022-10-21 15:43:31 +0200

    fixed UserSeeder

--- database/seeds/UserSeeder.php
+++ database/seeds/UserSeeder.php
@@ -17,6 +17,8 @@ class UserSeeder extends Seeder
             $new_user = new User();
 
             $new_user->name = $faker->name();
+            $new_user->first_name = $faker->firstName();
+            $new_user->last_name = $faker->lastName();
             $new_user->email = $faker->email();
             $new_user->password = bcrypt($faker->name());
 
