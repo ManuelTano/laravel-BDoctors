10da64ec7ebcec14af24e57e92d64605feb09deb
ManuelTano <manueltano98@gmail.com>
2022-10-24 15:59:48 +0200

    fix css

--- config/sponsorships.php
+++ config/sponsorships.php
@@ -5,16 +5,24 @@ return [
         'business_plan' => 'bronze',
         'time_plan' => 24,
         'price' => 2.99,
+        'color' => '#c7a48d',
+        'description' => 'Piano entry-level destinato per i medici nuovi alla piattaforma, consigliato a coloro che vogliono sperimentare una nuova esperienza. '
     ],
     [
         'business_plan' => 'silver',
         'time_plan' => 72,
         'price' => 5.99,
+        'color' => '#ccc4c1',
+        'description' => 'Il piano più acquistato dai nostri clienti. Offre il giusto compromesso tra visibilità e investimento.'
+
     ],
     [
         'business_plan' => 'gold',
         'time_plan' => 144,
         'price' => 9.99,
+        'color' => '#c3aa57',
+        'description' => 'Il nostro piano di punta. Completo di tutti i servizi, offre anche assistenza premium.'
+
     ],
     [
         'business_plan' => 'basic',
