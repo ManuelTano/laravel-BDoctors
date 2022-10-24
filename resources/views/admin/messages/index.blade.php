a2f52450945ec83d073a4357b4296d69261b041d
mauro-angel99 <angelottimauro@gmail.com>
2022-10-24 17:23:51 +0200

    change messages

--- resources/views/admin/messages/index.blade.php
+++ resources/views/admin/messages/index.blade.php
@@ -5,11 +5,15 @@
     <div class="container d-flex flex-wrap justify-content-between">
         @foreach ($messages as $message)
             @if ($message->user->id == Auth::id())
-                <div class="card mt-5" style="width: 18rem;">
-                    <div class="card-body">
-                        <h5 class="card-title">{{ $message->first_name }} {{ $message->last_name }}</h5>
-                        <p class="card-text">{{ $message->text }}</p>
-                        <p class="card-text">{{ $message->created_at }}</p>
+                <div class="card mt-5" style="width: 21rem;">
+                    <div class="card-body d-flex flex-column justify-content-between">
+                        <div>
+                            <h5 class="card-title">{{ $message->first_name }} {{ $message->last_name }}</h5>
+                            <p class="card-text">{{ $message->text }}</p>
+                        </div>
+                        <div class="mt-5">
+                            <p class="card-text">{{ $message->created_at }}</p>
+                        </div>
                     </div>
                 </div>
             @endif
