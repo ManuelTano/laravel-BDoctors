f76bac952909a7fe27b60e1f21f03224f09487da
PasqualePedoto <pasquale.pedoto123@gmail.com>
2022-10-26 17:45:31 +0200

    fixed api

--- routes/api.php
+++ routes/api.php
@@ -19,5 +19,5 @@ Route::middleware('auth:api')->get('/user', function (Request $request) {
 });
 
 Route::namespace('Api')->group(function(){
-    Route::recource('users', 'UserController');
+    Route::resource('users', 'UserController');
 });
