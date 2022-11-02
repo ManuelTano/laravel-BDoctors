3fd12256a64d9253dd4df3611e38ffdcccd38433
Aciar96 <alfredo.ciarmiello96@gmail.com>
2022-11-02 12:41:58 +0100

    fix card

---
+++ resources/js/components/pages/DoctorShow.vue
@@ -0,0 +1,37 @@
+<template>
+    <div>
+
+    </div>
+
+</template>
+
+<script>
+import BaseJumbotron from '../BaseJumbotron.vue';
+import BaseSelect from '../BaseSelect.vue';
+export default {
+    name: "DoctorShow",
+    props: {
+        specialties: Array,
+        text: String,
+    },
+    components: {
+        BaseJumbotron,
+        BaseSelect
+    },
+    data() {
+        return {
+            choice: "",
+        };
+    },
+    methods: {
+        emitQuery() {
+            this.$emit("doctors-for-specialty", this.choice);
+        },
+    },
+};
+
+</script>
+
+<style>
+
+</style>
