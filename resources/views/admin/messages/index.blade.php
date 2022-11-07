cd1ca9e650575d24843586f9f20164efa3fa3ad0
PasqualePedoto <pasquale.pedoto123@gmail.com>
2022-11-07 12:50:40 +0100

    fixed message

--- resources/views/admin/messages/index.blade.php
+++ resources/views/admin/messages/index.blade.php
@@ -8,11 +8,14 @@
             @if ($message->user->id == Auth::id())
             <div class="card d-flex justify-content-center col-12 mt-5 rounded-pill pl-5" style="width: 21rem;">
                 <div class="card-body d-flex flex-column justify-content-between">
-                    <div>
-                        <h5 class="card-title">{{ $message->first_name }} {{ $message->last_name }}</h5>
-                        <p class="card-text"><strong>Messaggio:</strong> {{ $message->text }}</p>
+                    <div class="d-flex flex-column justify-content-between">
+                        <div>
+                            <h5 class="card-title"><strong>{{ $message->first_name }} {{ $message->last_name }}</strong></h5>
+                            <h6 class="card-title text-muted">{{ $message->email }}</h6>
+                        </div>
+                        <p class="card-text mt-3"><strong>Messaggio:</strong> {{ $message->text }}</p>
                     </div>
-                    <div class="mt-5">
+                    <div class="mt-4">
                         <p class="card-text"><strong>Data ed orario:</strong> {{ $message->created_at }}</p>
                     </div>
                 </div>
