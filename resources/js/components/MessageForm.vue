9bcf933fdacb180feeac62f0c43a0041b039de5b
PasqualePedoto <pasquale.pedoto123@gmail.com>
2022-11-07 12:45:41 +0100

    commit

--- resources/js/components/MessageForm.vue
+++ resources/js/components/MessageForm.vue
@@ -148,7 +148,7 @@ export default {
 
             // Controllo che l'email sia valida
             if (
-                this.form.email &&
+                !this.form.email &&
                 !this.form.email.match(
                     /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/
                 )
